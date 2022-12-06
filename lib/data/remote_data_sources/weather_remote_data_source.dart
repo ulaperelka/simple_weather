0462994aaca0df353c1a34472922ae21c5a5c3b4
Ula Perelka <uperelka@gmail.com>
2022-12-06 18:07:32 +0100

    Implement  Data Source

---
+++ lib/data/remote_data_sources/weather_remote_data_source.dart
@@ -0,0 +1,11 @@
+import 'package:dio/dio.dart';
+
+class WeatherRemoteDataSource {
+  Future<Map<String, dynamic>?> getWeatherData({
+    required String city,
+  }) async {
+    final response = await Dio().get<Map<String, dynamic>>(
+        'http://api.weatherapi.com/v1/current.json?key=64acbc6a5a6944c58e5161232220612&q=$city&aqi=no');
+    return response.data;
+  }
+}
